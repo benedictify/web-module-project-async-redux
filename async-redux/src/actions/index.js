2bf1b0eddaa95808c5bc3c23ad8a3ba55daa8341
Benedict Schurwanz <benedictify@gmail.com>
2021-11-03 18:48:49 -0500

    add action creators for fetching data from API

---
+++ async-redux/src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+export const FETCH_START = "FETCH_START";
+export const FETCH_SUCCESS = "FETCH_SUCCESS";
+export const FETCH_ERROR = "FETCH_ERROR";
+
+export const fetchStart = () => {
+	return ({type: FETCH_START});
+}
+
+export const fetchSuccess = (data) => {
+	return ({type: FETCH_SUCCESS, payload: data});
+}
+
+export const fetchFail = (error) => {
+	return ({type: FETCH_ERROR, payload: error});
+}
