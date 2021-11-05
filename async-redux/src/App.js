61fc7fd9d8330f3e81cfcda29dc16dc28186d8d6
Benedict Schurwanz <benedictify@gmail.com>
2021-11-04 21:59:03 -0500

    create Comic component for displaying comic

--- async-redux/src/App.js
+++ async-redux/src/App.js
@@ -1,9 +1,15 @@
+import React from 'react';
+
+import Comic from './components/Comic';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
 			<h6 className="mini-header">Everything is okay.</h6>
+
+			<Comic />
     </div>
   );
 }
---
+++ async-redux/src/components/Comic.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+
+import { getCurrentComic } from '../actions';
+
+const Comic = () => {
+	return (
+		<div>
+			<h2>Comic Component</h2>
+		</div>
+	)
+}
+
+export default Comic;
