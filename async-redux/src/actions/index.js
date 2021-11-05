f6aa417db5c20bee0a0181224cf72e9fab95ddf9
Benedict Schurwanz <benedictify@gmail.com>
2021-11-04 21:58:30 -0500

    create fetch actions and reducer cases

--- async-redux/src/actions/index.js
+++ async-redux/src/actions/index.js
@@ -4,8 +4,21 @@ export const FETCH_START = "FETCH_START";
 export const FETCH_SUCCESS = "FETCH_SUCCESS";
 export const FETCH_ERROR = "FETCH_ERROR";
 
-export const getData = () => {
-	return null;
+export const getCurrentComic = () => {
+	// axios call to get data from server
+	// get data of current comic
+	return (dispatch) => {
+		dispatch(fetchStart());
+
+		axios.get('https://xkcd.com/info.0.json')
+			.then(response => {
+				dispatch(fetchSuccess(response.data))
+				console.log(response)
+			})
+			.catch(error => {
+				dispatch(fetchFail(error))
+			});
+	}
 }
 
 export const fetchStart = () => {
--- async-redux/src/reducers/index.js
+++ async-redux/src/reducers/index.js
@@ -1,18 +1,40 @@
-import { FETCH_START } from "../actions";
+import { FETCH_ERROR, FETCH_START, FETCH_SUCCESS } from "../actions";
 
 
 const initialState = {
 	displayComic:	{},
-	maxComicNumber: 1
+	maxComicNumber: 1,
+	fetching: false,
+	errorMsg: ''
 }
 
 export const reducer = (state = initialState, action) => {
 	switch (action.type) {
 		case (FETCH_START):
+			// fetching true, clear comic data, no error
 			return ({
-				...state
+				...state,
+				fetching: true,
+				displayComic: {},
+				error: ''
 			});
-		
+		case (FETCH_SUCCESS): 
+			// save returned data to store, not fetching, no error
+			return ({
+				...state,
+				displayComic: action.payload,
+				maxComicNumber: action.payload.num,
+				fetching: false,
+				error: ''
+			});
+		case (FETCH_ERROR): 
+			// payload will contain error message, not fetching, no comic data
+			return ({
+				...state,
+				displayComic: {}, 
+				fetching: false,
+				error: action.payload
+			})
 		default:
 			return state;
 	}
